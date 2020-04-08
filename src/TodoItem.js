897698ce34d1828f21c8e658654178c220a35e4b
Franci Nakamura <francinakamura@FranciNrasiMac3.attlocal.net>
2020-04-07 22:39:02 -0700

    added onChange and handler

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,21 +2,21 @@ import React from "react"
 
 function TodoItem (props) {
     return (
-        <div className = "todo-item">
-        
-        <p>
-        
-            <input type="checkbox" checked = {props.item.completed} />
-               {props.item.text}
-            
-        </p>
+        <div className = "todo-item">           
+           
+           <p>
+           
+            <input 
+                type="checkbox" 
+                checked = {props.item.completed} 
+                onChange = {() => props.handleChange(props.item.id)}/>
+            {props.item.text}
+            </p>
             
 
         </div>    
     )
 }
-
-
 
 
 export default TodoItem
