6e30d15a4ac28be16e12172e35ec99d9a2844256
Franci Nakamura <francinakamura@FranciNrasiMac3.attlocal.net>
2020-04-07 22:22:35 -0700

    added ability to check & uncheck boxes

--- src/App.js
+++ src/App.js
@@ -10,13 +10,28 @@ class App extends React.Component {
        todos: todosData
 
      }
+  this.handleChange = this.handleChange.bind (this)
   }
-
+handleChange (id) {
+  this.setState (prevState => {
+    let updatedTodos = prevState.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      } 
+      return todo    
+    })
+      return {
+        todos: updatedTodos
+      }
+  })
+}
   render () {
-    let todoItems = this.state.todos.map(item=> <TodoItem key={item.id} item = {item} />)
+    let todoItems = this.state.todos.map(item=> <TodoItem key={item.id} item = {item} 
+    handleChange = {this.handleChange}/>)
+
   return (
     
-      <div> 
+      <div className ="todo-list"> 
         
         <h1>To Do List</h1>
         {todoItems}
